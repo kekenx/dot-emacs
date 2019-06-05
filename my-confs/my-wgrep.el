91576971c8e393a585b57cf680610aaae26d4d71
kekenx <kekenxx@gmail.com>
2019-06-05 12:32:40 +0900

    ...

---
+++ my-confs/my-wgrep.el
@@ -0,0 +1,9 @@
+;;; my-wgrep.el --- my wgrep configuration
+;;; Commentary:
+;;; wgrep --- wgrep
+
+;;; Code:
+(ensure-package-installed 'wgrep)
+
+(require 'wgrep nil t)
+;;; my-wgrep.el ends here
--- my-confs/my-yasnippet.el
+++ my-confs/my-yasnippet.el
@@ -5,14 +5,9 @@
 ;;; Code:
 (ensure-package-installed 'yasnippet-snippets)
 
-(add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/site-lisp/yasnippet"))
-
 (require 'yasnippet)
-;; (setq yas-snippet-dirs
-;;       '("~/emacs/my-snippets"
-;; 	"~/.emacs.d/snippets"
-;;         ))
+(setq yas-snippet-dirs (append yas-snippet-dirs
+                               '("~/emacs/my-snippets")))
 
 (yas-global-mode 1)
 
