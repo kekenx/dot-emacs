1e8b1bc2736fb1db852cf7d941f0ccc7476677d0
Kenichiro Kanai <kekenx@kekenx-mbp.local>
2020-09-29 08:03:13 +0900

    add rust config

--- my-confs/my-rust.el
+++ my-confs/my-rust.el
@@ -3,10 +3,23 @@
 ;;; python configuration --- ...
 
 ;;; Code:
-(ensure-package-installed 'rust-mode 'flycheck-rust)
+(ensure-package-installed 'rust-mode 'flycheck-rust 'racer)
 
-(with-eval-after-load 'rust-mode
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+;;; Add path to the compiler, racer and rustfmt
+(add-to-list 'exec-path (expand-file-name "~/.cargo/bin/"))
+;;; auto format on save
+(eval-after-load "rust-mode"
+  '(setq-default rust-format-on-save t))
 
+(add-hook 'rust-mode-hook (lambda ()
+                            (racer-mode)
+                            (flycheck-rust-setup)))
+;;; eldoc suppor on racer
+(add-hook 'racer-mode-hook #'eldoc-mode)
+;;; racer auto-complete
+(add-hook 'racer-mode-hook (lambda ()
+                             (company-mode)
+                             (set (make-variable-buffer-local 'company-idle-delay) 0.1)
+                             (set (make-variable-buffer-local 'company-minimum-prefix-length) 0)))
 
 ;;; my-rust.el ends here
