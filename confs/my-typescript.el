ac2c213017f32b9c3549a7107ee2f29d75e5dc2e
kekenx <kekenxx@gmail.com>
2022-10-10 15:14:55 +0900

    typescript

--- confs/my-typescript.el
+++ confs/my-typescript.el
@@ -1,71 +1,67 @@
 ;;; my-typescript.el --- typescript support
 ;;; Commentary: reference https://qiita.com/nuy/items/ebcb25ad14f02ab72790
 ;;; Code:
-(use-package typescript-mode
+;; (use-package typescript-mode
+;;   :config
+;;   (setq typescript-indent-level 2)
+;;   (add-hook 'typescript-mode-hook
+;;           (lambda ()
+;;             (interactive)
+;;             (mmm-mode)
+;;             )))
+
+;; (use-package mmm-mode
+;;   :commands mmm-mode
+;;   :mode (("\\.tsx\\'" . typescript-mode))
+;;   :config
+;;   (setq mmm-global-mode t)
+;;   (setq mmm-submode-decoration-level 0)
+;;   (mmm-add-classes
+;;    '((mmm-jsx-mode
+;;       :submode web-mode
+;;       :face mmm-code-submode-face
+;;       :front "\\(return\s\\|n\s\\|(\n\s*\\)<"
+;;       :front-offset -1
+;;       :back ">\n?\s*)\n}\n"
+;;       :back-offset 1
+;;       )))
+;;   (mmm-add-mode-ext-class 'typescript-mode nil 'mmm-jsx-mode)
+
+
+;;   (defun mmm-reapply ()
+;;     (mmm-mode)
+;;     (mmm-mode))
+
+;;   (add-hook 'after-save-hook
+;;             (lambda ()
+;;               (when (string-match-p "\\.tsx?" buffer-file-name)
+;;                 (mmm-reapply)
+;;                 )))
+;;   )
+
+(use-package web-mode
   :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.ts\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+
   :config
-  (setq typescript-indent-level 2)
-  (add-hook 'typescript-mode-hook
-          (lambda ()
-            (interactive)
-            (mmm-mode)
-            )))
+  (setq web-mode-attr-indent-offset nil)
 
-(use-package mmm-mode
-  :ensure t
-  :commands mmm-mode
-  :mode (
-	 ("\\.tsx\\'" . typescript-mode)
-	 ("\\.vue\\'" . typescript-mode)
-	 )
-  :config
-  (setq mmm-global-mode t)
-  (setq mmm-submode-decoration-level 0)
-  (set-face-background 'mmm-default-submode-face "gray13")
+  (setq web-mode-enable-auto-closing t)
+  (setq web-mode-enable-auto-pairing t)
 
-  (mmm-add-classes
-   '((mmm-jsx-mode
-      :submode web-mode
-      :face mmm-code-submode-face
-      :front "\\(return\s\\|n\s\\|(\n\s*\\)<"
-      :front-offset -1
-      :back ">\n?\s*)\n}\n"
-      :back-offset 1
-      )
-     (vue-embeded-slim-mode
-      :submode slim-mode
-      :front "^<template.*lang=\"pug\">\n"
-      :back "^</template>"
-      )
-     (vue-embeded-web-mode
-      :submode web-mode
-      :front "^<template>\n"
-      :back "^</template>\n"
-      )
-     (vue-embeded-js-mode
-      :submode js-mode
-      :front "^<script>\n"
-      :back "^</script>"
-      )
-     (vue-embeded-scss-mode
-      :submode scss-mode
-      :front "^<style.*lang=\"scss\">\n"
-      :back "^</style>")))
-  (mmm-add-mode-ext-class 'typescript-mode nil 'mmm-jsx-mode)
-  (mmm-add-mode-ext-class 'typescript-mode "\\.vue\\'" 'vue-embeded-slim-mode)
-  (mmm-add-mode-ext-class 'typescript-mode "\\.vue\\'" 'vue-embeded-web-mode)
-  (mmm-add-mode-ext-class 'typescript-mode "\\.vue\\'" 'vue-embeded-js-mode)
-  (mmm-add-mode-ext-class 'typescript-mode "\\.vue\\'" 'vue-embeded-scss-mode)
+  (setq web-mode-auto-close-style 2)
+  (setq web-mode-tag-auto-close-style 2)
 
 
-  (defun mmm-reapply ()
-    (mmm-mode)
-    (mmm-mode))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
 
-  (add-hook 'after-save-hook
-            (lambda ()
-              (when (string-match-p "\\.tsx?" buffer-file-name)
-                (mmm-reapply)
-                )))
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2)
   )
+
+
 ;;; my-typescript.el ends here~
