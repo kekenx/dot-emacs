cecb9e2662e27fa8308cb72fa973284a0a78e87d
kekenx <kekenxx@gmail.com>
2023-07-06 16:32:07 +0900

    add terraform

--- confs/my-terraform.el
+++ confs/my-terraform.el
@@ -5,6 +5,12 @@
 ;;; Code:
 (use-package terraform-mode
   :ensure t
+  :custom terraform-format-on-save t
 )
 
+(use-package company-terraform
+  :ensure t
+  :init
+  (company-terraform-init)
+)
 ;; my-terraform.el ends here
