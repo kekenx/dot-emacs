39ab83a4d04cb7107adea2db1953ef9498431c29
kekenx <kekenxx@gmail.com>
2023-10-04 22:29:52 +0900

    add magit

---
+++ confs/my-magit.el
@@ -0,0 +1,13 @@
+;;; my-magit.el --- magit configuration
+;;; Commentary: 
+;;; magit configuration --- ...
+
+;;; Code:
+
+(use-package magit
+  :ensure t
+  :bind (("C-c C-c C-g" . magit-status))
+  )
+
+
+;; my-magit.el ends here
