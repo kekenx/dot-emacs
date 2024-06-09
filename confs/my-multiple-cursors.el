bad9538fa59e546fa91bc125324c896c58056401
kekenx <kekenxx@gmail.com>
2024-06-09 14:49:44 +0900

    use different shortcuts

--- confs/my-multiple-cursors.el
+++ confs/my-multiple-cursors.el
@@ -8,8 +8,8 @@
   :ensure t
   :init
   (global-set-key (kbd "C-c m c") 'mc/edit-lines)
-  (global-set-key (kbd "C-o") 'mc/mark-next-like-this)
-  (global-set-key (kbd "C-u") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-m") 'mc/mark-next-like-this)
+  ;; (global-set-key (kbd "C-u") 'mc/mark-previous-like-this)
   (global-set-key (kbd "C-c C-i") 'mc/mark-all-like-this))
 
 ;;; my-multiple-cursors.el ends here
