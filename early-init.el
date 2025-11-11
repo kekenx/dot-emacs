4781e5725180973b0320c5480bfdd7901949c8e6
kekenx <ken.kanai@gotofu.com>
2025-11-11 22:07:31 +0900

    Add claude code

---
+++ early-init.el
@@ -0,0 +1,8 @@
+;;; early-init.el --- -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; Disable package loading via package.el
+(setq package-enable-at-startup nil)
+
+;;; early-init.el ends here
